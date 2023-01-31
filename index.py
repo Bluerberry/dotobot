3f8869ed9b3fe4d8ff905095e28e67358dd9768c
Bluerberry <boazkreulen.main@gmail.com>
2023-01-31 14:15:22 +0100

    Basic bot bootup

--- index.py
+++ index.py
@@ -1,11 +1,31 @@
 
+# Logging setup
 import logging
 from logging import config
 
 config.fileConfig('log.conf')
 logger = logging.getLogger('root')
 
-logger.info('info message')
-logger.warning('warn message')
-logger.error('error message')
-logger.critical('critical message')
+# Environment setup
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Discord setup
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    logger.info(f'Succesful login as {client.user}')
+
+@client.event
+async def on_message(msg):
+    if msg.content == 'ping':
+        await msg.channel.send('pong')
+        logger.info(f'{msg.author} played a match of ping-pong!')
+
+client.run(os.getenv('DISCORD_TOKEN'))
