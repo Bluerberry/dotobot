573242c3471a7a5a0848f4943d0034ed60e21d77
Bluerberry <boazkreulen.main@gmail.com>
2023-01-31 18:28:47 +0100

    Added some extension utility

--- extensions/example.py
+++ extensions/example.py
@@ -6,19 +6,19 @@ from discord.ext import commands
 
 # ---------------------> Logging setup
 
-name = basename(__file__)
+name = basename(__file__)[:-2]
 log = logging.getLogger(name)
 
 # ---------------------> Example cog
 
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(Example(bot))
-    log.info(f'Cog has been created: {name}')
+    log.info(f'Extension has been created: {name}')
 
 def teardown(bot: commands.Bot) -> None:
-    log.info(f'Extention has been destroyed: {name}')
+    log.info(f'Extension has been destroyed: {name}')
 
-class Example(commands.Cog, name = name, description = 'An example Cog'):
+class Example(commands.Cog, name = name, description = 'An example extension'):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
     
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 import discord
 from discord.ext import commands
-from discord import ExtensionAlreadyLoaded
+from discord import ExtensionAlreadyLoaded, ExtensionNotLoaded, ExtensionNotFound
 
 # ---------------------> Logging setup
 
@@ -21,12 +21,178 @@ bot = commands.Bot(command_prefix = '$', intents = intents)
 
 @bot.event
 async def on_ready() -> None:
-    log.info(f'Succesful login as {bot.user}')      
+    log.info(f'Succesful login as {bot.user}')
+
+@bot.command()
+async def load(ctx: commands.Context, *args) -> None:
+    
+    # Flags
+    all: bool = '--all' in args
+    silent: bool = '--silent' in args
+    verbose: bool = '--verbose' in args
+
+    # Preperations
+    total: int = 0
+    success: int = 0
+    summary: str = ''
+
+    if not args or all:
+        args = ['extensions.' + file[:-3] for file in listdir('./extensions') if file.endswith('.py')]
+    else:
+        args = ['extensions.' + arg for arg in args if not arg.startswith('--')]
+
+    # Load extensions
+    for arg in args:
+
+        try:
+            bot.load_extension(arg)
+            summary += f'🟢 {arg} sucessfully loaded\n'
+
+        except ExtensionAlreadyLoaded as err:
+            log.warning(err)
+            summary += f'🟡 {arg} was already loaded\n'
+
+        except ExtensionNotFound as err:
+            log.warning(err)
+            summary += f'🟠 {arg} doesn\'t exist\n'
+
+        except Exception as err:
+            log.error(err)
+            summary += f'🔴 {arg} failed to load\n'
+        
+        else:
+            success += 1
+        finally:
+            total += 1
+    
+    # TODO Maybe use discord pages for a nicer interface
+    # Feedback
+    if not silent:
+        if total == 0:
+            await ctx.channel.send(f'No extensions have loaded')
+            return
+        if verbose:
+            await ctx.channel.send(f'```{summary}```')
+        if success == total:
+            await ctx.channel.send(f'All extensions have loaded')
+        else:
+            await ctx.channel.send(f'{success} out of {total} extensions have loaded')
+
+@bot.command()
+async def unload(ctx: commands.Context, *args) -> None:
+   
+    # Flags
+    all: bool = '--all' in args
+    silent: bool = '--silent' in args
+    verbose: bool = '--verbose' in args
+
+    # Preperations
+    total: int = 0
+    success: int = 0
+    summary: str = ''
+
+    if not args or all:
+        args = list(bot.extensions.keys())
+    else:
+        args = ['extensions.' + arg for arg in args if not arg.startswith('--')]
+
+    # Unload extensions
+    for arg in args:
+
+        try:
+            bot.unload_extension(arg)
+            summary += f'🟢 {arg} sucessfully unloaded\n'
+
+        except ExtensionNotLoaded as err:
+            log.warning(err)
+            summary += f'🟡 {arg} was already unloaded\n'
+
+        except ExtensionNotFound as err:
+            log.warning(err)
+            summary += f'🟠 {arg} doesn\'t exist\n'
+
+        except Exception as err:
+            log.error(err)
+            summary += f'🔴 {arg} failed to unload\n'
+        
+        else:
+            success += 1
+        finally:
+            total += 1
+    
+    # TODO Maybe use discord pages for a nicer interface
+    # Feedback
+    if not silent:
+        if total == 0:
+            await ctx.channel.send(f'No extensions have unloaded')
+            return
+        if verbose:
+            await ctx.channel.send(f'```{summary}```')
+        if success == total:
+            await ctx.channel.send(f'All extensions have unloaded')
+        else:
+            await ctx.channel.send(f'{success} out of {total} extensions have unloaded')
+
+@bot.command()
+async def reload(ctx: commands.Context, *args) -> None:
+      
+    # Flags
+    all: bool = '--all' in args
+    silent: bool = '--silent' in args
+    verbose: bool = '--verbose' in args
+
+    # Preperations
+    total: int = 0
+    success: int = 0
+    summary: str = ''
+
+    if all:
+        args = list(bot.extensions.keys())
+    else:
+        args = ['extensions.' + arg for arg in args if not arg.startswith('--')]
+
+    # Reload extensions
+    for arg in args:
+
+        try:
+            bot.reload_extension(arg)
+            summary += f'🟢 {arg} sucessfully reloaded\n'
+
+        except ExtensionNotLoaded as err:
+            log.warning(err)
+            summary += f'🟡 {arg} wasn\'t loaded\n'
+
+        except ExtensionNotFound as err:
+            log.warning(err)
+            summary += f'🟠 {arg} doesn\'t exist\n'
+
+        except Exception as err:
+            log.error(err)
+            summary += f'🔴 {arg} failed to reload\n'
+        
+        else:
+            success += 1
+        finally:
+            total += 1
+    
+    # TODO Maybe use discord pages for a nicer interface
+    # Feedback
+    if not silent:
+        if total == 0:
+            await ctx.channel.send(f'No extensions have reloaded')
+            return
+        if verbose:
+            await ctx.channel.send(f'```{summary}```')
+        if success == total:
+            await ctx.channel.send(f'All extensions have reloaded')
+        else:
+            await ctx.channel.send(f'{success} out of {total} extensions have reloaded')
+
 
 # ---------------------> Main
 
 if __name__ == '__main__':
-    for ext in ['cogs.' + file[:-3] for file in listdir('./cogs') if file.endswith('.py')]:
+    for ext in ['extensions.' + file[:-3] for file in listdir('./extensions') if file.endswith('.py')]:
         try:
             bot.load_extension(ext)
         except ExtensionAlreadyLoaded as err:
